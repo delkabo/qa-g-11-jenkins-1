782f5b0ba29533af16b2de906c48799dc86c6ac1
delkabo <delkabo@yandex.ru>
2022-03-15 22:53:28 +0300

    first commit add project from ga-g-10

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,7 +2,7 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import helpers.Attach;
+import heplers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
